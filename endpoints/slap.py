c458ccb71d614319240647a524c1a9192e4a2b9b
R C N <plbwymw@gmail.com>
2021-06-19 16:56:04 +0300

    ds

--- endpoints/slap.py
+++ endpoints/slap.py
@@ -11,15 +11,36 @@ from utils.endpoint import Endpoint, setup
 class Slap(Endpoint):
     params = ['avatar0', 'avatar1']
 
-    def generate(self, avatars, text, usernames, kwargs):
-        base = Image.open(self.assets.get('assets/batslap/batslap.bmp')).resize((1000, 500)).convert('RGBA')
-        avatar = http.get_image(avatars[1]).resize((220, 220)).convert('RGBA')
-        avatar2 = http.get_image(avatars[0]).resize((200, 200)).convert('RGBA')
-        base.paste(avatar, (580, 260), avatar)
-        base.paste(avatar2, (350, 70), avatar2)
-        base = base.convert('RGB')
+    def generate(self, avatars, kwargs):
+        sprite = Image.open(self.assets.get('assets/petpet/hand.png')).resize((1000, 500)).convert('RGBA')
+        avatar = http.get_image(avatars[0]).resize((200, 200)).convert('RGBA')
+        sprite.paste(avatar, (580, 260), avatar)
+        sprite = sprite.convert('RGB')
 
+        images = []
         b = BytesIO()
-        base.save(b, format='png')
+
+        images[0].save(
+            fp,
+            "GIF",
+            save_all=True,
+            append_images=images[1:],
+            loop=0,
+            disposal=2,
+        )
+
+        for index in range(5):
+            im = Image.new("RGBA", (100, 100), None)
+            im.paste(avatars[0], (25, 25), avatars[0])
+            im.paste(sprite, (0 - (112 * index), 0), sprite)
+            images.append(im)
+        sprite.close()
+
         b.seek(0)
+
+        
+        
+
+        sprite.save(b, format='png')
+        
         return send_file(b, mimetype='image/png')
